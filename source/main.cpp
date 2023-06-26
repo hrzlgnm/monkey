41dbeb528b60304a0592132f687796d9f156dc12
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-06-26 23:53:50 +0200

    feat: fix todos and read-parse-print-loop

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,36 @@
 #include <future>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "lexer.hpp"
+#include "parser.hpp"
 
 constexpr auto prompt = ">> ";
+
+constexpr auto monkey_face = R"r(
+             __,__
+     .--. .-"     "-. .--.
+    / .. \/ .-. .-. \/ .. \
+   | |  '| /   Y   \ |'  | |
+   | \   \ \ 0 | 0 / /  /  |
+    \ '-,\.-"""""""-./,-' /
+     ''-' /_  ^ ^ _ \ '-''
+         | \._   _./ |
+         \  \ '~' / /
+          '._'-=-'_.'
+            '-----'
+)r";
+
+auto print_parse_errors(const std::vector<std::string>& errors)
+{
+    std::cerr << monkey_face;
+    std::cerr << "Woops! We ran into some monkey business here!\n";
+    std::cerr << "  parser errorrs: \n";
+    for (const auto& error : errors) {
+        std::cerr << "\t" << error << "\n";
+    }
+}
 
 auto main() -> int
 {
@@ -13,11 +39,13 @@ auto main() -> int
     auto input = std::string {};
     while (getline(std::cin, input)) {
         auto lxr = lexer {input};
-        auto tok = lxr.next_token();
-        while (tok.type != token_type::eof) {
-            std::cout << tok << '\n';
-            tok = lxr.next_token();
+        auto prsr = parser {lxr};
+        auto prgrm = prsr.parse_program();
+        if (!prsr.errors().empty()) {
+            print_parse_errors(prsr.errors());
+            continue;
         }
+        std::cout << prgrm->string() << "\n";
         std::cout << prompt;
     }
 
