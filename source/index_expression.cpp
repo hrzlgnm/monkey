1fac5df762b8625a97d9357a729c405c17371ae2
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-08-01 19:39:30 +0200

    f

---
+++ source/index_expression.cpp
@@ -0,0 +1,15 @@
+#include "index_expression.hpp"
+
+#include "environment.hpp"
+#include "object.hpp"
+
+auto index_expression::string() const -> std::string
+{
+    return fmt::format("({}[{}])", left->string(), index->string());
+}
+
+auto index_expression::eval(environment_ptr env) const -> object
+{
+    unused(env);
+    return {};
+}
---
+++ source/index_expression.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "expression.hpp"
+
+struct index_expression : expression
+{
+    using expression::expression;
+    auto string() const -> std::string override;
+    auto eval(environment_ptr env) const -> object override;
+
+    expression_ptr left;
+    expression_ptr index;
+};
