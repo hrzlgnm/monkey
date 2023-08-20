439e0e3c1337d05de270087a34f00d33d989a6f2
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-08-20 13:26:25 +0200

    simplify format/lint targets

--- cmake/lint-targets.cmake
+++ cmake/lint-targets.cmake
@@ -1,13 +1,5 @@
 set(FORMAT_PATTERNS
-    source/*.cpp
-    source/*.hpp
-    source/**/*.cpp
-    source/**/*.hpp
-    include/*.hpp
-    test/*.cpp
-    test/*.hpp
-    test/**/*.cpp
-    test/**/*.hpp
+    **/*.cpp **/*.hpp
     CACHE STRING
           "; separated patterns relative to the project source dir to format")
 
