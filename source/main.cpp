76d8a7e0b1bc2c18b1bf5d562127083bda990afe
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-08-20 20:53:23 +0200

    feat: support reading files (#54)

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,6 @@
+#include <cstdlib>
 #include <exception>
+#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <memory>
@@ -45,32 +47,42 @@ auto print_parse_errors(const std::vector<std::string>& errors)
     }
 }
 
-enum class run_mode
+enum class engine
 {
-    compile,
-    interpret,
+    vm,
+    eval,
 };
 
-struct cmdline_options
+struct command_line_args
 {
     bool help {};
     bool debug_env {};
-    run_mode mode {};
+    engine mode {};
     std::string_view file;
 };
 
-auto parse_command_line(int argc, char** argv) -> cmdline_options
+[[noreturn]] auto show_usage(std::string_view program, std::string_view error_msg = {})
 {
-    cmdline_options opts {};
+    auto exit_code = EXIT_SUCCESS;
+    if (!error_msg.empty()) {
+        fmt::print("Error: {}\n", error_msg);
+        exit_code = EXIT_FAILURE;
+    }
+    fmt::print("Usage: {} [-d] [-i] [-h] [<file>]\n\n", program);
+    exit(exit_code);
+}
+
+auto parse_command_line(std::string_view program, int argc, char** argv) -> command_line_args
+{
+    command_line_args opts {};
     for (std::string_view arg : std::span(argv, static_cast<size_t>(argc))) {
         if (arg.at(0) == '-' && arg.size() == 1) {
-            fmt::print("WARNING: skipping unknown command line arg {}\n", arg);
-            continue;
+            show_usage(program, fmt::format("invalid option {}", arg));
         }
         if (arg.at(0) == '-' && arg.size() > 1) {
             switch (arg.at(1)) {
                 case 'i':
-                    opts.mode = run_mode::interpret;
+                    opts.mode = engine::eval;
                     break;
                 case 'h':
                     opts.help = true;
@@ -78,82 +90,121 @@ auto parse_command_line(int argc, char** argv) -> cmdline_options
                 case 'd':
                     opts.debug_env = true;
                     break;
+                default: {
+                    show_usage(program, fmt::format("invalid option {}", arg));
+                }
             }
         } else {
             if (opts.file.empty()) {
                 opts.file = arg;
+            } else {
+                fmt::print("ignoring file argument {}, already have one set.\n", arg);
             }
         }
     }
     return opts;
 }
 
-auto show_usage(std::string_view program, std::string_view error_msg = {})
+auto run_file(const command_line_args& opts) -> int
 {
-    if (!error_msg.empty()) {
-        fmt::print("Error: {}\n", error_msg);
-    }
-    fmt::print("Usage: {} [-d] [-i] [-h] [<file>]\n\n", program);
-}
-
-auto main(int argc, char* argv[]) -> int
-{
-    auto program = std::string_view(*argv);
-    auto opts = parse_command_line(argc - 1, ++argv);
-    if (opts.help) {
-        show_usage(program);
-        return 0;
-    }
-    if (!opts.file.empty()) {
-        show_usage(program, "file is not supported yet, sorry");
+    std::ifstream ifs(std::string {opts.file});
+    if (!ifs) {
+        std::cerr << "ERROR: could not open file: " << opts.file << "\n";
         return 1;
     }
-    auto input = std::string {};
-    std::cout << prompt;
+    std::string contents {(std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>())};
+    auto lxr = lexer {contents};
+    auto prsr = parser {lxr};
+    auto prgrm = prsr.parse_program();
+    if (!prsr.errors().empty()) {
+        print_parse_errors(prsr.errors());
+        return 1;
+    }
+    if (opts.mode == engine::vm) {
+        auto cmplr = compiler::create();
+        cmplr.compile(prgrm);
+        auto machine = vm::create(cmplr.byte_code());
+        machine.run();
+        auto result = machine.last_popped();
+        if (!result.is_nil()) {
+            std::cout << std::to_string(result.value) << "\n";
+        }
+    } else {
+        auto global_env = std::make_shared<environment>();
+        for (const auto& builtin : builtin_function_expression::builtins) {
+            global_env->set(builtin.name, object {bound_function(&builtin, environment_ptr {})});
+        }
+        auto evaluated = prgrm->eval(global_env);
+        if (!evaluated.is_nil()) {
+            std::cout << std::to_string(evaluated.value) << "\n";
+        }
+        global_env->break_cycle();
+    }
+    return 0;
+}
+
+auto run_repl(const command_line_args& opts) -> int
+{
     auto global_env = std::make_shared<environment>();
     auto symbols = symbol_table::create();
-
+    auto consts = std::make_shared<constants>();
+    auto globals = std::make_shared<constants>(globals_size);
     for (auto idx = 0UL; const auto& builtin : builtin_function_expression::builtins) {
         global_env->set(builtin.name, object {bound_function(&builtin, environment_ptr {})});
         symbols->define_builtin(idx, builtin.name);
         idx++;
     }
-    auto consts = std::make_shared<constants>();
-    auto globals = std::make_shared<constants>(globals_size);
-
-    try {
-        auto cache = std::vector<statement_ptr>();
-        while (getline(std::cin, input)) {
-            auto lxr = lexer {input};
-            auto prsr = parser {lxr};
-            auto prgrm = prsr.parse_program();
-            if (!prsr.errors().empty()) {
-                print_parse_errors(prsr.errors());
-                continue;
-            }
-            if (opts.mode == run_mode::compile) {
-                auto cmplr = compiler::create_with_state(consts, symbols);
-                cmplr.compile(prgrm);
-                auto machine = vm::create_with_state(cmplr.byte_code(), globals);
-                machine.run();
-                auto result = machine.last_popped();
-                std::cout << std::to_string(result.value) << "\n";
-            } else {
-                auto evaluated = prgrm->eval(global_env);
-                if (!evaluated.is_nil()) {
-                    std::cout << std::to_string(evaluated.value) << "\n";
-                }
-            }
-            std::move(prgrm->statements.begin(), prgrm->statements.end(), std::back_inserter(cache));
-            if (opts.debug_env) {
-                global_env->debug();
-            }
-            std::cout << prompt;
+    auto cache = std::vector<statement_ptr>();
+    auto input = std::string {};
+    std::cout << prompt;
+    while (getline(std::cin, input)) {
+        auto lxr = lexer {input};
+        auto prsr = parser {lxr};
+        auto prgrm = prsr.parse_program();
+        if (!prsr.errors().empty()) {
+            print_parse_errors(prsr.errors());
+            continue;
         }
-        global_env->break_cycle();
+        if (opts.mode == engine::vm) {
+            auto cmplr = compiler::create_with_state(consts, symbols);
+            cmplr.compile(prgrm);
+            auto machine = vm::create_with_state(cmplr.byte_code(), globals);
+            machine.run();
+            auto result = machine.last_popped();
+            std::cout << std::to_string(result.value) << "\n";
+        } else {
+            auto evaluated = prgrm->eval(global_env);
+            if (!evaluated.is_nil()) {
+                std::cout << std::to_string(evaluated.value) << "\n";
+            }
+        }
+        std::move(prgrm->statements.begin(), prgrm->statements.end(), std::back_inserter(cache));
+        if (opts.debug_env) {
+            global_env->debug();
+        }
+        std::cout << prompt;
+    }
+    global_env->break_cycle();
+    return 0;
+}
+
+auto main(int argc, char* argv[]) -> int
+{
+    auto program = std::string_view(*argv);
+    auto opts = parse_command_line(program, argc - 1, ++argv);
+    if (opts.help) {
+        show_usage(program);
+    }
+    try {
+        if (!opts.file.empty()) {
+            return run_file(opts);
+        }
+        return run_repl(opts);
+
     } catch (const std::exception& e) {
         monkey_business();
         std::cerr << "Caught an exception: " << e.what() << "\n";
+        return 1;
     }
     return 0;
 }
