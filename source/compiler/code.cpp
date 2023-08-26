91a80be738cb4f61b8647d906e91f8029ade1a2b
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-08-26 14:19:48 +0200

    ast tests

--- source/compiler/code.cpp
+++ source/compiler/code.cpp
@@ -209,95 +209,98 @@ auto flatten(const std::vector<std::vector<T>>& arrs) -> std::vector<T>
     return result;
 }
 
-TEST_CASE("[code] make")
+TEST_SUITE("code")
 {
-    struct test
+    TEST_CASE("make")
     {
-        opcodes opcode;
-        operands opers;
-        instructions expected;
-    };
-    using enum opcodes;
-    std::array tests {
-        test {
-            constant,
-            {65534},
-            {static_cast<uint8_t>(constant), 255, 254},
-        },
-        test {
-            add,
-            {},
-            {static_cast<uint8_t>(add)},
-        },
-        test {
-            pop,
-            {},
-            {static_cast<uint8_t>(pop)},
-        },
-        test {
-            get_local,
-            {255},
-            {static_cast<uint8_t>(get_local), 255},
-        },
-        test {
-            closure,
-            {65534, 255},
-            {static_cast<uint8_t>(closure), 255, 254, 255},
-        },
-    };
-    for (auto&& [opcode, operands, expected] : tests) {
-        auto actual = make(opcode, operands);
-        REQUIRE_EQ(actual, expected);
-    };
-}
+        struct test
+        {
+            opcodes opcode;
+            operands opers;
+            instructions expected;
+        };
+        using enum opcodes;
+        std::array tests {
+            test {
+                constant,
+                {65534},
+                {static_cast<uint8_t>(constant), 255, 254},
+            },
+            test {
+                add,
+                {},
+                {static_cast<uint8_t>(add)},
+            },
+            test {
+                pop,
+                {},
+                {static_cast<uint8_t>(pop)},
+            },
+            test {
+                get_local,
+                {255},
+                {static_cast<uint8_t>(get_local), 255},
+            },
+            test {
+                closure,
+                {65534, 255},
+                {static_cast<uint8_t>(closure), 255, 254, 255},
+            },
+        };
+        for (auto&& [opcode, operands, expected] : tests) {
+            auto actual = make(opcode, operands);
+            REQUIRE_EQ(actual, expected);
+        };
+    }
 
-TEST_CASE("[code] instructionsToString")
-{
-    const auto* const expected = R"(0000 OpAdd
+    TEST_CASE("instructionsToString")
+    {
+        const auto* const expected = R"(0000 OpAdd
 0001 OpGetLocal 1
 0003 OpConstant 2
 0006 OpConstant 65535
 0009 OpClosure 65535 255
 )";
-    std::vector<instructions> instrs {
-        make(opcodes::add),
-        make(opcodes::get_local, 1),
-        make(opcodes::constant, 2),
-        make(opcodes::constant, 65535),
-        make(opcodes::closure, {65535, 255}),
-    };
-    auto concatenated = flatten(instrs);
-    auto actual = to_string(concatenated);
-    REQUIRE_EQ(expected, actual);
-}
+        std::vector<instructions> instrs {
+            make(opcodes::add),
+            make(opcodes::get_local, 1),
+            make(opcodes::constant, 2),
+            make(opcodes::constant, 65535),
+            make(opcodes::closure, {65535, 255}),
+        };
+        auto concatenated = flatten(instrs);
+        auto actual = to_string(concatenated);
+        REQUIRE_EQ(expected, actual);
+    }
 
-TEST_CASE("[code] readOperands")
-{
-    struct test
+    TEST_CASE("readOperands")
     {
-        opcodes opcode;
-        operands opers;
-        int bytes_read;
-    };
+        struct test
+        {
+            opcodes opcode;
+            operands opers;
+            int bytes_read;
+        };
 
-    std::array tests {
-        test {
-            opcodes::constant,
-            {65534},
-            2,
-        },
-    };
-    for (auto&& [opcode, operands, bytes] : tests) {
-        const auto instr = make(opcode, operands);
-        const auto def = lookup(opcode);
+        std::array tests {
+            test {
+                opcodes::constant,
+                {65534},
+                2,
+            },
+        };
+        for (auto&& [opcode, operands, bytes] : tests) {
+            const auto instr = make(opcode, operands);
+            const auto def = lookup(opcode);
 
-        REQUIRE(def.has_value());
+            REQUIRE(def.has_value());
 
-        const auto actual = read_operands(def.value(), {instr.begin() + 1, instr.end()});
-        CHECK_EQ(actual.second, bytes);
-        for (size_t iidx = 0; iidx < operands.size(); ++iidx) {
-            INFO("at", iidx);
-            CHECK_EQ(operands[iidx], actual.first[iidx]);
+            const auto actual = read_operands(def.value(), {instr.begin() + 1, instr.end()});
+            CHECK_EQ(actual.second, bytes);
+            for (size_t iidx = 0; iidx < operands.size(); ++iidx) {
+                INFO("at", iidx);
+                CHECK_EQ(operands[iidx], actual.first[iidx]);
+            }
         }
     }
 }
