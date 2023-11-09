0d342c8cd84a7f5cf151632ae894d54f5addad4c
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-11-09 23:18:28 +0100

    cleanup

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,9 +1,5 @@
 include(FetchContent)
 
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
 FetchContent_Declare(fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt.git
     GIT_TAG 8.1.1
