d73a9f9cfdbb4d465fffed659def3358c37dd8ff
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-15 23:34:58 +0100

    feat: great user and handle exceptions in repl (#112)

--- source/main.cpp
+++ source/main.cpp
@@ -11,12 +11,12 @@
 #include <vector>
 
 #include <ast/builtin_function_expression.hpp>
-#include <gc.hpp>
 #include <compiler/compiler.hpp>
 #include <compiler/symbol_table.hpp>
 #include <eval/environment.hpp>
 #include <fmt/base.h>
 #include <fmt/format.h>
+#include <gc.hpp>
 #include <lexer/lexer.hpp>
 #include <parser/parser.hpp>
 #include <vm/vm.hpp>
@@ -44,7 +44,7 @@ constexpr auto monkey_face = R"r(
 auto monkey_business()
 {
     std::cerr << monkey_face;
-    std::cerr << "Woops! We ran into some monkey business here!\n";
+    std::cerr << "Woops! We ran into some monkey business here!\n  ";
 }
 
 auto print_parse_errors(const std::vector<std::string>& errors)
@@ -52,8 +52,23 @@ auto print_parse_errors(const std::vector<std::string>& errors)
     monkey_business();
     std::cerr << "  parser errors: \n";
     for (const auto& error : errors) {
-        std::cerr << "\t" << error << "\n";
+        std::cerr << "    " << error << '\n';
     }
+}
+
+auto show_error(std::string_view error_kind, std::string_view error_message)
+{
+    std::cerr << "Whoops! We ran into some " << error_kind << " error: \n  " << error_message << '\n';
+}
+
+auto print_compile_error(std::string_view error)
+{
+    show_error("compiler", error);
+}
+
+auto print_eval_error(std::string_view error)
+{
+    show_error("evaluation", error);
 }
 
 enum class engine : std::uint8_t
@@ -62,6 +77,17 @@ enum class engine : std::uint8_t
     eval,
 };
 
+auto operator<<(std::ostream& strm, engine en) -> std::ostream&
+{
+    switch (en) {
+        case engine::vm:
+            return strm << "vm";
+        case engine::eval:
+            return strm << "eval";
+    }
+    return strm << "unknown";
+}
+
 struct command_line_args
 {
     bool help {};
@@ -69,6 +95,19 @@ struct command_line_args
     engine mode {};
     std::string_view file;
 };
+
+auto get_logged_in_user() -> std::string
+{
+    // NOLINTBEGIN(concurrency-mt-unsafe)
+    const char* username = getenv("USER");
+
+    if (username == nullptr) {
+        username = getenv("USERNAME");
+    }
+    // NOLINTEND(concurrency-mt-unsafe)
+
+    return (username != nullptr) ? std::string(username) : "Unknown";
+}
 
 [[noreturn]] auto show_usage(std::string_view program, std::string_view error_msg = {})
 {
@@ -120,7 +159,7 @@ auto run_file(const command_line_args& opts) -> int
 {
     std::ifstream ifs(std::string {opts.file});
     if (!ifs) {
-        std::cerr << "ERROR: could not open file: " << opts.file << "\n";
+        std::cerr << "ERROR: could not open file: " << opts.file << '\n';
         return 1;
     }
     const std::string contents {(std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>())};
@@ -138,7 +177,7 @@ auto run_file(const command_line_args& opts) -> int
         machine.run();
         const auto* result = machine.last_popped();
         if (!result->is(object::object_type::null)) {
-            std::cout << result->inspect() << "\n";
+            std::cout << result->inspect() << '\n';
         }
     } else {
         auto* global_env = make<environment>();
@@ -147,7 +186,7 @@ auto run_file(const command_line_args& opts) -> int
         }
         const auto* result = prgrm->eval(global_env);
         if (!result->is(object::object_type::null)) {
-            std::cout << result->inspect() << "\n";
+            std::cout << result->inspect() << '\n';
         }
     }
     return 0;
@@ -155,44 +194,61 @@ auto run_file(const command_line_args& opts) -> int
 
 auto run_repl(const command_line_args& opts) -> int
 {
+    std::cout << "Hello " << get_logged_in_user() << ". This is the Monkey programming language using engine "
+              << opts.mode << ".\n";
+    std::cout << "Feel free to type in commands\n";
     auto* global_env = make<environment>();
     auto* symbols = symbol_table::create();
     constants consts;
     constants globals(globals_size);
     for (auto idx = 0UL; const auto& builtin : builtin_function_expression::builtins) {
-        global_env->set(builtin->name, make<function_object>(builtin, nullptr));
+        global_env->set(builtin->name, make<builtin_object>(builtin));
         symbols->define_builtin(idx, builtin->name);
         idx++;
     }
+    auto show_prompt = []() { std::cout << prompt; };
     auto input = std::string {};
-    std::cout << prompt;
+    show_prompt();
     while (getline(std::cin, input)) {
         auto lxr = lexer {input};
         auto prsr = parser {lxr};
         auto* prgrm = prsr.parse_program();
         if (!prsr.errors().empty()) {
             print_parse_errors(prsr.errors());
+            show_prompt();
             continue;
         }
         if (opts.mode == engine::vm) {
-            auto cmplr = compiler::create_with_state(&consts, symbols);
-            cmplr.compile(prgrm);
-            auto machine = vm::create_with_state(cmplr.byte_code(), &globals);
-            machine.run();
-            const auto* result = machine.last_popped();
-            if (result != nullptr && !result->is(object::object_type::null)) {
-                std::cout << result->inspect() << "\n";
+            try {
+                auto cmplr = compiler::create_with_state(&consts, symbols);
+                cmplr.compile(prgrm);
+                auto machine = vm::create_with_state(cmplr.byte_code(), &globals);
+                machine.run();
+                const auto* result = machine.last_popped();
+                if (result != nullptr && !result->is(object::object_type::null)) {
+                    std::cout << result->inspect() << '\n';
+                }
+            } catch (const std::exception& e) {
+                print_compile_error(e.what());
+                show_prompt();
+                continue;
             }
         } else {
-            const auto* result = prgrm->eval(global_env);
-            if (result != nullptr && !result->is(object::object_type::null)) {
-                std::cout << result->inspect() << "\n";
+            try {
+                const auto* result = prgrm->eval(global_env);
+                if (result != nullptr && !result->is(object::object_type::null)) {
+                    std::cout << result->inspect() << '\n';
+                }
+            } catch (const std::exception& e) {
+                print_eval_error(e.what());
+                show_prompt();
+                continue;
             }
         }
         if (opts.debug_env) {
             global_env->debug();
         }
-        std::cout << prompt;
+        show_prompt();
     }
     return 0;
 }
@@ -213,7 +269,7 @@ auto main(int argc, char* argv[]) -> int
 
     } catch (const std::exception& e) {
         monkey_business();
-        std::cerr << "Caught an exception: " << e.what() << "\n";
+        std::cerr << "Caught an exception: " << e.what() << '\n';
         return 1;
     }
 }
