81eaeb3ba5fa2404edcf311272bf32ecbd3d003f
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-15 14:17:43 +0100

    Shadow const

--- source/ast/function_expression.cpp
+++ source/ast/function_expression.cpp
@@ -5,7 +5,7 @@
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
-function_expression::function_expression(std::vector<std::string>&& params, statement* bod)
+function_expression::function_expression(std::vector<std::string>&& params, const statement* bod)
     : callable_expression(std::move(params))
     , body {bod}
 {
