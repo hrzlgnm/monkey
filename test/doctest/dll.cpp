5066e015d8e9697167265299232d06ff460f71fa
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-24 17:26:35 +0100

    Exclude doctest from misc-include-cleaner

--- test/doctest/dll.cpp
+++ test/doctest/dll.cpp
@@ -1,3 +1,5 @@
 #define DOCTEST_CONFIG_IMPLEMENTATION_IN_DLL
 #define DOCTEST_CONFIG_IMPLEMENT
+// NOLINTBEGIN(misc-include-cleaner)
 #include <doctest/doctest.h>
+// NOLINTEND(misc-include-cleaner)
