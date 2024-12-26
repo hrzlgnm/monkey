22b6d754fc4ff078fdf522c5e4b4ac8deececa1a
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-26 03:10:36 +0100

    include style

--- source/analyzer/analyzer.hpp
+++ source/analyzer/analyzer.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "ast/program.hpp"
-#include "compiler/symbol_table.hpp"
+#include <ast/program.hpp>
+#include <compiler/symbol_table.hpp>
 
 void analyze_program(const program* program, symbol_table* existing_symbols) noexcept(false);
 [[noreturn]] void fail(const std::string& error_message);
