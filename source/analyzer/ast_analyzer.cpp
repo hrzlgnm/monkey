f67ed6ed033ce1ea94ab2ac868edf8eb7309c697
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-26 02:10:53 +0100

    remove unused testing stuff

--- source/analyzer/ast_analyzer.cpp
+++ source/analyzer/ast_analyzer.cpp
@@ -171,22 +171,6 @@ auto call_expression::check(analyzer& anlzr, symbol_table* symbols) const -> voi
 namespace
 {
 // NOLINTBEGIN(*)
-template<typename T>
-auto maker(std::initializer_list<T> list) -> std::vector<T>
-{
-    return std::vector<T> {list};
-}
-
-template<typename T>
-auto flatten(const std::vector<std::vector<T>>& arrs) -> std::vector<T>
-{
-    std::vector<T> result;
-    for (const auto& arr : arrs) {
-        std::copy(arr.cbegin(), arr.cend(), std::back_inserter(result));
-    }
-    return result;
-}
-
 auto check_no_parse_errors(const parser& prsr) -> bool
 {
     INFO("expected no errors, got:", fmt::format("{}", fmt::join(prsr.errors(), ", ")));
