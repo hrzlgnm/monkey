56236376ed9c180978a6c35b704ca152eb909810
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-12-26 03:51:00 +0100

    fix: interpreter not using previous globals in as check

--- source/analyzer/analyzer.hpp
+++ source/analyzer/analyzer.hpp
@@ -2,6 +2,9 @@
 
 #include <ast/program.hpp>
 #include <compiler/symbol_table.hpp>
+#include <eval/environment.hpp>
 
-void analyze_program(const program* program, symbol_table* existing_symbols) noexcept(false);
+void analyze_program(const program* program,
+                     symbol_table* existing_symbols,
+                     const environment* existing_env) noexcept(false);
 [[noreturn]] void fail(const std::string& error_message);
